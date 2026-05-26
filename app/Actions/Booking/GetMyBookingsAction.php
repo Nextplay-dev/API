f50d73f1f1d63a13840fb4afd4e05925f117b3b4
Nicolas Jacquemin <nicolas@rigidd.app>
2026-05-26 12:44:41 +0200

    feat: allow null emails for booking guests, include user relation in bookings, and add BookingInvitationPolicy

--- app/Actions/Booking/GetMyBookingsAction.php
+++ app/Actions/Booking/GetMyBookingsAction.php
@@ -12,7 +12,7 @@ class GetMyBookingsAction
     public function handle(User $user): LengthAwarePaginator
     {
         return QueryBuilder::for($user->bookings())
-            ->with('activity', 'resource.venue', 'guests.user')
+            ->with('activity', 'resource.venue', 'guests.user', 'user')
             ->allowedFilters([
                 AllowedFilter::scope('upcoming'),
             ])
