6299b1c5467fda16d7f1aa0afecb2dfa4f8878eb
Nicolas Jacquemin <nicolas@rigidd.app>
2026-05-27 14:29:21 +0200

    feat: add me profile and stats endpoints to user authentication routes

---
+++ app/Http/Controllers/Auth/GetMeStatsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Actions\User\GetMeStatsAction;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class GetMeStatsController extends Controller
+{
+    public function __invoke(GetMeStatsAction $action): JsonResponse {
+        $user = Auth::user();
+        return response()->json($action->handle($user));
+    }
+}
